a1397819a3d82e7dfc9f22782a37d48da49f15de
Nezoonezxv <92717383+Nezoonezxv@users.noreply.github.com>
2022-03-13 05:16:52 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -3,9 +3,11 @@ const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
 1. *Nezoone Support*
-https://chat.whatsapp.com/CDjyfZIMM6HDkpxsxGp5ed
+https://chat.whatsapp.com/Fh0iUeYo79T9IlYM0tiCMM
 2. *Link GC Nna*
 https://chat.whatsapp.com/EMHJmVLRlAjEZMKKaRh1Bo
+3. *Link GC Supp 3*
+https://chat.whatsapp.com/GvANYqr07aHH6lIDyvwKQ0
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
